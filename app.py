44ce559dee3fefa6577e972e3f5e63a6a1bfcbde
21IT157 <97622620+21IT157@users.noreply.github.com>
2023-09-28 23:07:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,37 @@
-# Include PIL, load_image before main()
-from PIL import Image
+import streamlit as st
 
-def load_image(image_file):
-	img = Image.open(image_file)
-	return img
+st.title("Mango Leaf Diseases Detection")
 
-...
+# Button for Image Upload
+upload_button = st.button("Upload Image")
 
-if choice == "Image":
-		st.subheader("Image")
-		image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+# Button for Opening Camera
+open_camera_button = st.button("Open Camera")
 
-		if image_file is not None:
+# Button to remove the image
+remove_image_button = st.button("Remove Image")
 
-			  # To See details
-			  file_details = {"filename":image_file.name, "filetype":image_file.type,
-                              "filesize":image_file.size}
-			  st.write(file_details)
+# Placeholder for displaying the selected image
+image_container = st.empty()
 
-              # To View Uploaded Image
-			  st.image(load_image(image_file),width=250)
+if upload_button:
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+
+    if uploaded_file is not None:
+        st.image(uploaded_file, caption="Image Preview", use_column_width=True)
+        image_container.image(uploaded_file, caption="Image Preview", use_column_width=True)
+image = Image.open(uploaded_file)
+
+
+
+#displaying the image on streamlit app
+
+st.image(image, caption='Enter any caption here')
+if open_camera_button:
+    st.write("Opening Camera...")  # Placeholder for opening the camera
+
+if remove_image_button:
+    image_container.empty()
+
+# Note: The JavaScript functions for previewImage, openCamera, and removeImage are not needed in Streamlit
+# as Streamlit handles the interactivity directly on the server side.
